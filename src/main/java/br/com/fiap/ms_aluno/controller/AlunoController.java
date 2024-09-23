1a4e9a630b205dc256ee4b9b10c3fad11030b173
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-23 11:33:00 -0300

    Aluno controller testing

--- src/main/java/br/com/fiap/ms_aluno/controller/AlunoController.java
+++ src/main/java/br/com/fiap/ms_aluno/controller/AlunoController.java
@@ -1,9 +1,26 @@
 package br.com.fiap.ms_aluno.controller;
 
+import br.com.fiap.ms_aluno.dto.AlunoDTO;
+import br.com.fiap.ms_aluno.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoService service;
+
+    @GetMapping
+    public ResponseEntity<List<AlunoDTO>> findAll(){
+        List<AlunoDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
 }
