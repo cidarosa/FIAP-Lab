f63e52d7ec461f9e4434dafe703acf2d941c30e9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-23 07:46:10 -0300

    Init projeto

---
+++ src/test/java/br/com/fiap/ms_aluno/MsAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ms_aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
